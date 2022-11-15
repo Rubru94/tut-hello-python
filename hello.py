0a9da76da40a2242efc97d39d49e652bd6079b29
Ruben Rubio <Ruben.Rubio@ust.com>
2022-11-15 14:16:26 +0100

    feat: Infer types

--- hello.py
+++ hello.py
@@ -1,8 +1,15 @@
 # Hello world
-print("Hello world!!")
+print('Hello world!!')
 
 # Oneline comment
 
-"""
+'''
 Multiple lines comment
-"""
+'''
+
+# Types
+print(type('Hello world!!')) # Type 'str'
+print(type(2)) # Type 'int'
+print(type(2.2)) # Type 'float'
+print(type(True)) # Type 'bool'
+print(type(1 + 2j)) # Type 'complex'
